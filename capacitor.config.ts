2db4e228be5bf1970c1d58452ffcac505bfe21a4
Vincent Agwu <vincentagwu@gmail.com>
2024-08-22 10:49:53 +0200

    Forked project to implement manioc allergy scanner

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'capacitor.ocr.example',
-  appName: 'capacitor-ocr-example',
+  appName: 'Huak Tuah',
   webDir: 'www'
 };
 
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { Ocr, TextDetections } from '@capacitor-community/image-to-text';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -15,7 +16,119 @@ export class HomePage {
   message: string = '';
   busy = false;
 
-  constructor(private sanitizer: DomSanitizer) { }
+  huakTuahList: string[] = [
+    "Taro",
+    "Colocasia esculenta",
+    "Wasserbrotwurzel",
+    "Kolokasie",
+    "Taioba",
+    "Eddo",
+    "Eddoe",
+    "Eddro",
+    "Dasheen",
+    "Cocoyam",
+    "Macabo",
+    "Mankani",
+    "Koko",
+    "Lambo",
+    "Ede",
+    "Tarro",
+    "Quequisque",
+    "Malanga",
+    "Papa china",
+    "Arbi",
+    "Sato-imo",
+    "Elefantenohrpflanze",
+    "Alocasia macrorrhizos",
+    "Tannia",
+    "Cyrtosperma merkusii",
+    "Kalo",
+    "Poi",
+    "Katchu",
+    "Amadumbe",
+    "Dalo",
+    "Keladi",
+    "Mwanza",
+    "Nduma",
+    "Abalongi",
+    "Suran",
+    "Ghandyali",
+    "Tales",
+    "Inhame",
+    "Chembu",
+    "Kochu",
+    "Gabi",
+    "Eddos",
+    "Madumbi",
+    "Ube",
+    "Khoai môn",
+    "Bun long",
+    "Pindalai",
+    "Yautia",
+    "Yautia coco",
+    "Uhi",
+    "Kalo (Hawaii)",
+    "Manioc",
+    "Cassava",
+    "Yuca",
+    "Mandioca",
+    "Manihot esculenta",
+    "Ape",
+    "Kape",
+    "Ufi",
+    "Gari",
+    "Mukape",
+    "Mihogo",
+    "Mboboch",
+    "Sanku",
+    "Xivuti",
+    "Ubikayu",
+    "Balinghoy",
+    "Kamoteng Kahoy",
+    "Manioc doux",
+    "Amani",
+    "Kappa",
+    "Rogo",
+    "Akpu",
+    "Bitter Cassava",
+    "Sweet Cassava",
+    "Mandubi",
+    "Kapok",
+    "Katela",
+    "Farinha",
+    "Fufu",
+    "Lafun",
+    "Kisamvu",
+    "Mogo",
+    "Plátano macho",
+    "Saka-Saka",
+    "Yuca dulce",
+    "Roti",
+    "Bibai",
+    "Kpukpuru",
+    "Tikap",
+    "Nampi",
+    "Mahu",
+    "Huti",
+    "Thar",
+    "Mbegui",
+    "Maphuthu",
+    "Mboshi",
+    "Rik",
+    "Pouna",
+    "Quyavi",
+    "Buchu",
+    "Saka",
+    "Kivunde",
+    "Yapo",
+    "Ngai",
+    "Ntoi"
+  ];
+
+  constructor(
+    private sanitizer: DomSanitizer,
+    private alertController: AlertController // Hinzufügen des AlertControllers
+  ) { }
 
   async takePic() {
     try {
@@ -32,6 +145,10 @@ export class HomePage {
       const path = photo.path;
       if (!path) return;
       this.lines = await this.processImage(path);
+
+      // Überprüfen, ob eines der Wörter in huakTuahList in den erkannten Zeilen vorkommt
+      await this.checkForHuakTuahWords();
+
     } catch (err) {
       alert(err);
     } finally {
@@ -41,17 +158,51 @@ export class HomePage {
 
   async processImage(filename: string): Promise<string[]> {
     const data: TextDetections = await Ocr.detectText({ filename });
-    // or with orientation -
-    // const textDetections = await td.detectText({ filename, orientation: ImageOrientation.Up })
-
     console.log(data);
     const result: string[] = [];
     for (let detection of data.textDetections) {
       result.push(detection.text); 
-      //`${detection.text} (${detection.bottomLeft},${detection.bottomRight},${detection.topLeft},${detection.topRight})`);
     }
     this.message = result.length === 0 ? 'No text was detected' : '';
     return result;
+  }
+
+  async checkForHuakTuahWords() {
+    for (let line of this.lines) {
+      for (let word of this.huakTuahList) {
+        if (line.includes(word)) {
+          await this.showAlertFound(`Das Wort "${word}" wurde erkannt!`);
+          return;// Alert anzeigen und beenden, wenn eines der Wörter gefunden wurde
+        }
+      }
+    }
+    await this.showAlertNotFound('Keines der gesuchten Wörter wurde erkannt.');
+  }
+
+  async showAlertFound(message: string) {
+    const alert = await this.alertController.create({
+      header: 'Halt Stop!',
+      subHeader: 'Vergiftungsgefahr! Lebensmittel nicht essen!',
+
+      // CSS-Stile für den Alert-Hintergrund
+      message: message,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  async showAlertNotFound(message: string) {
+    const alert = await this.alertController.create({
+      header: 'Keine Gefahr!',
+      subHeader: 'Du kannst das Lebensmittel essen!',
+
+      // CSS-Stile für den Alert-Hintergrund
+      message: message,
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
 
 }
